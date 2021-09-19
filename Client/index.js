1571bc031e67f1f53e48947d069ffb958443d6ca
Cryptobyte <me@cryptobyte.dev>
2021-09-19 15:59:15 -0400

    Add rate limit to react app server

--- Client/index.js
+++ Client/index.js
@@ -2,6 +2,20 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const rateLimit = require('express-rate-limit');
+
+/*
+ * 250 requests every 5 minutes, you may want to 
+ * change this if you're using this in production 
+ * for some reason..
+ */
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 250
+});
+
+app.use(limiter);
+
 // Setup static directories for React app
 app.use(express.static(path.join(__dirname, 'app', 'build')));
 app.use(express.static(path.join(__dirname, 'app', 'public')));
